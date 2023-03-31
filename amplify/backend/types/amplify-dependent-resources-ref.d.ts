684ef619ee1840b279f3047e997a444635fbc8e5
Kevin Connor <kevinpaulconnor@gmail.com>
2023-03-30 22:25:12 -0500

    amplify cli and package updates

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "wordlegolff32179f3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "updateBoard": {
+      "Arn": "string",
+      "CloudWatchEventRule": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "tournamentsResource": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
